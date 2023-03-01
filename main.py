1a8111ca64b07c8d2a58f2a4265dc591a4ead19d
Dave <davenyambune@gmail.com>
2023-03-01 15:22:47 +0300

    Registerd students to my database

--- main.py
+++ main.py
@@ -1,1 +1,50 @@
-print("Hello")
+from peewee import *
+from os import path
+
+connection = path.dirname(path.realpath(__file__))
+db = SqliteDatabase(path.join(connection, "Dave.db"))
+
+#Creating our first table
+class Student(Model):
+    stud_name = CharField()
+    stud_email = CharField()
+    stud_password = CharField()
+
+    class Meta:
+        database = db
+
+Student.create_table(fail_silently=True)
+
+
+class Teacher(Model):
+    teacher_name = CharField()
+    teacher_email = CharField()
+    teacher_Password = CharField()
+
+    class Meta:
+        database = db
+
+Teacher.create_table(fail_silently=True)
+
+class Product(Model):
+    product_price = CharField()
+    product_quantity = CharField()
+    product_description = CharField()
+    product_color = CharField()
+
+    class Meta:
+        database = db
+
+Product.create_table(fail_silently=True)
+
+
+class User(Model):
+    user_name = CharField()
+    user_phone = CharField()
+    user_email = CharField()
+    user_password = CharField()
+
+    class Meta:
+        database = db
+
+User.create_table(fail_silently=True)
